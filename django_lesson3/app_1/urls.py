593efec4deb4e41e3d6cbf43c60168aa0367672d
Abbos <abbosr180@gmail.com>
2022-09-12 23:10:23 +0500

    lesson 4 end

--- django_lesson3/app_1/urls.py
+++ django_lesson3/app_1/urls.py
@@ -1,1 +1,7 @@
+from django.urls import path
+from . views import *
 
+
+urlpatterns = [
+    path('', get_employess, name='employess_list')
+]
--- django_lesson3/app_1/views.py
+++ django_lesson3/app_1/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Employees
 
-# Create your views here.
+
+def get_employess(request):
+    queryset = Employees.objects.only('first_name ')
+    print("\n", queryset.query)
+    str_data = ""
+    for i in queryset:
+        str_data += f"<li>{i}</li>"
+    return HttpResponse(f"<ul>{str_data}</ul>")
