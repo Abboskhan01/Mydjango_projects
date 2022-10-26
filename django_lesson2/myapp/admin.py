6722c56a9141c6f6ac0ace1409cbb4fa4cac2310
Abbos <abbosr180@gmail.com>
2022-10-26 19:12:50 +0500

    added new models

--- django_lesson2/myapp/admin.py
+++ django_lesson2/myapp/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from .models import Category, Author, Book, Person
+from django.apps import apps
+# from .models import Category, Author, Book, Person
 
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(Person)
+admin.site.register(apps.all_models['myapp'].values())
